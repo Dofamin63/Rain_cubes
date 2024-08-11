6c0861bf49c081fad08566c39b7535c158be57c5
Beloskov <88579232+Dofamin63@users.noreply.github.com>
2024-08-11 18:38:41 +0700

    Initial commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private int _minLiveTime;
+    [SerializeField] private int _maxLiveTime;
+
+    private WaitForSeconds _lifeTime;
+    private Color _defaultColor;
+    private Renderer _renderer;
+
+    public event Action<Cube> TimeIsUp; 
+
+    private void Awake()
+    {
+        _defaultColor = Color.white;
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_renderer.material.color == _defaultColor)
+        {
+            _renderer.material.color = Random.ColorHSV();
+        }
+
+        StartCoroutine(Life());
+    }
+
+    private IEnumerator Life()
+    {
+        _lifeTime = new WaitForSeconds(Random.Range(_minLiveTime, _maxLiveTime));
+        yield return _lifeTime;
+        SetDefaultColor();
+        TimeIsUp?.Invoke(this);
+    }
+    
+    private void SetDefaultColor()
+    {
+        _renderer.material.color = _defaultColor;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Pool;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Cube _prefab;
+    [SerializeField] private float _delay;
+
+    private ObjectPool<Cube> _pool;
+    private WaitForSeconds _spawnDelay;
+    private int _minPosition = -23;
+    private int _maxPosition = 23;
+    private int _height = 15;
+    
+    private void Awake()
+    {
+        _pool = new ObjectPool<Cube>(
+            createFunc: () => Instantiate(_prefab),
+            actionOnGet: (obj) => OnGet(obj),
+            actionOnRelease: (obj) => obj.gameObject.SetActive(false),
+            actionOnDestroy: (obj) => Destroy(obj));
+
+        _spawnDelay = new WaitForSeconds(_delay);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (enabled)
+        {
+            _pool.Get();
+            yield return _spawnDelay;
+        }
+    }
+    
+    private void OnGet(Cube cube)
+    {
+        cube.TimeIsUp += OnRelease;
+        cube.transform.position = new Vector3(Random.Range(_minPosition, _maxPosition), _height, Random.Range(_minPosition, _maxPosition));
+        cube.gameObject.SetActive(true);
+    }
+
+    private void OnRelease(Cube cube)
+    {
+        cube.TimeIsUp -= OnRelease;
+        _pool.Release(cube);
+    }
+}
